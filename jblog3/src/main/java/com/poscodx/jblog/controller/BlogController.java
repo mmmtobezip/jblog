679f73194883ab5e77b213e3c264238678c1eec1
bitacademy-poscodx <poscoict.bitacademy@gmail.com>
2024-06-13 15:05:13 +0900

    jblog #2

--- jblog3/src/main/java/com/poscodx/jblog/controller/BlogController.java
+++ jblog3/src/main/java/com/poscodx/jblog/controller/BlogController.java
@@ -5,15 +5,35 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/{id:(?!assets).*}")
 public class BlogController {
 	
-	@RequestMapping({"/{id}","/{id}/{categoryNo}", "/{id}/{categoryNo}/{postNo}" })
+	@RequestMapping({"", "/{categoryNo}", "/{categoryNo}/{postNo}" })
 	public String index(
 		@PathVariable("id") String id,
 		@PathVariable("categoryNo") Long categoryNo,
 		@PathVariable("postNo") Long postNo) {
-		
+
 		return "blog/main";
+	}
+	
+	// @Auth
+	@RequestMapping("/admin/basic")
+	public String adminBasic(@PathVariable("id") String id) {
+		return "blog/admin-basic";
+	}
+
+	// @Auth
+	@RequestMapping("/admin/category")
+	public String adminCategory(@PathVariable("id") String id) {
+		
+		return "blog/admin-category";
+	}
+	
+	//@Auth
+	@RequestMapping("/admin/write")
+	public String adminWrite(@PathVariable("id") String id) {
+		return "blog/admin-write";
 	}
 
 }
